cde8ea7ca7a108d8e6f2b406f82f7d6018cb9b27
Péter Kalicz <kaliczp@gmail.com>
2017-02-28 15:28:15 +0100

    generate daily from smoothed

--- process.R
+++ process.R
@@ -37,12 +37,31 @@ monthly.max <- round(apply.monthly(LAI.x1,max),1)
 month.num <- as.numeric(format(index(monthly.max),"%m"))
 month.recod <- ifelse(month.num < 5,13,month.num)
 month.ws <- ifelse(month.recod >= 10,"winter","summer")
+month.wsc <- ifelse(month.num == 4 | month.num == 10,"change",NA)
+month.wsc <- ifelse(is.na(month.wsc),month.ws,month.wsc)
 
-LAI.df <- data.frame(month=month.ws,max=as.numeric(monthly.max),min=as.numeric(apply.monthly(LAI.x1,min)))
-o
-LAI.df[LAI.df$month == "summer",3] <- NA
-LAI.df[LAI.df$month == "winter",2] <- NA
+monthly.min=as.numeric(apply.monthly(LAI.x1,min))
+monthly.mean=round(as.numeric(apply.monthly(LAI.x1,mean)),1)
 
-LAI.maxmin <- xts(ifelse(is.na(LAI.df[,2]),LAI.df[,3],LAI.df[,2]),index(monthly.max))
+LAI.df <- data.frame(month=month.wsc,max=as.numeric(monthly.max),
+                     min=monthly.min,
+                     mean=monthly.mean)
+LAI.df[LAI.df$month == "change",c(2,3)] <- NA
+LAI.df[LAI.df$month == "summer",3:4] <- NA
+LAI.df[LAI.df$month == "winter",c(2,4)] <- NA
+
+LAI.monthraw <- ifelse(is.na(LAI.df[,2]),LAI.df[,3],LAI.df[,2])
+LAI.monthraw <- ifelse(is.na(LAI.monthraw),LAI.df[,4],LAI.monthraw)
+summary(monthly.max)
+date.new <- seq(as.Date("2002-07-15"),as.Date("2016-12-15"),by="months")
+LAI.maxmin <- xts(LAI.monthraw,date.new)
 plot(LAI.x, col=2)
 lines(LAI.maxmin)
+
+LAIann(LAI.x,2016)
+lines(LAI.maxmin,lwd=2)
+
+## Daily
+date.new <- seq(as.Date("2002-07-15"),as.Date("2016-12-15"),by="days")
+LAI.d <- xts(rep(NA,length(date.new)),date.new)
+LAI.d <- round(na.approx(merge.xts(LAI.d,LAI.maxmin)[,2]),1)
